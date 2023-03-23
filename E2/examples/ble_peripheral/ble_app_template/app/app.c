4ddc2a907b1eb4c0f0d915c7697cadbeaa4b03ec
xiebiao <biao.x@augustint.com>
2023-03-23 17:58:03 +0800

    修改固件版本号。

--- E2/examples/ble_peripheral/ble_app_template/app/app.c
+++ E2/examples/ble_peripheral/ble_app_template/app/app.c
@@ -13,7 +13,7 @@
 #include "app_timer.h"
 #include "app_uart.h"
 
-#define FW_VERSION	"E2_NRF52810_FW_V1.0.2_20221124"
+#define FW_VERSION	"E2_NRF52810_FW_V1.0.3_20230322"
 
 bool connect_flag;
 bool judg_app_flag;
